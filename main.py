df7d496d1708a2d59b2388d8b9d3eca072ba648e
Swoje <dgrutter@gmail.com>
2025-05-16 16:57:18 +0000

    asteroids done

--- main.py
+++ main.py
@@ -3,22 +3,59 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
+import sys
+from shot import Shot
+
 
 def main ():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
-    running = True
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
+
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+    running = True  #not present in solution files
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)  
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collisionCheck(shot) == True:
+                    asteroid.split()
+                    shot.kill()
+
+        for asteroid in asteroids:
+            if asteroid.collisionCheck(player) == True:
+                print("Game over!")
+                sys.exit()
         screen.fill((0,0,0))
+        #screen.fill("black")  from solution files
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
         
 
 if __name__ == "__main__":
